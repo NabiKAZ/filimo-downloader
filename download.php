de614b7d707629175df13958b4913ec1c4cc574c
Nabi <nabikaz@gmail.com>
2017-12-17 04:16:02 +0330

    initial commit of filimo downloader and stats
    Signed-off-by: Nabi <nabikaz@gmail.com>

---
+++ download.php
@@ -0,0 +1,167 @@
+<?php
+echo "Filimo Downloader - version 0.1.0 - Copyright 2017\n";
+echo "By Nabi KaramAliZadeh <www.nabi.ir> <nabikaz@gmail.com>\n";
+echo "Signup here: http://filimo.com/invite/NabiKAZ/a8ca\n";
+echo "Project link: https://github.com/NabiKAZ/filimo-downloader\n";
+echo "===========================================================\n";
+
+$config_path = 'config/';
+$base_path = 'download/';
+$proxy = '';
+
+login:
+@mkdir($config_path);
+$contents = get_contents('https://www.filimo.com/');
+preg_match('/username: \'(.*?)\',/', $contents, $match);
+if ($match[1] != '0') {
+    echo "Your username: $match[1]\n";
+} else {
+    echo "> Login to filimo.com\n";
+    echo "Input username: ";
+    $username = trim(fgets(STDIN));
+    $contents = get_contents('https://www.filimo.com/etc/api/signinstep1/usernamemo/' . $username . '/devicetype/desktop');
+    $response = json_decode($contents);
+    if ($response->signinstep1->type != 'success') {
+        die("Error: " . $response->signinstep1->value . "\n");
+    } else {
+        $tempid = @$response->signinstep1->tempid;
+        echo "Input password: ";
+        $password = trim(fgets(STDIN));
+        echo "Logging...\n";
+        $contents = get_contents('https://www.filimo.com/etc/api/signinstep2/usernamemo/' . $username . '/codepass/' . $password . '/tempid/' . $tempid . '/usernamemotype/username/');
+        $response = json_decode($contents);
+        if ($response->signinstep2->type != 'success') {
+            die("Error: " . $response->signinstep2->value . "\n");
+        } else {
+            echo "Logged In.\n";
+            echo "IMPORTANT: Your private cookies is stored in the '$config_path' directory, Be careful about its security!\n";
+            echo "===========================================================\n";
+            goto login;
+        }
+    }
+}
+echo "===========================================================\n";
+
+echo "Input Video ID: ";
+$video_id = trim(fgets(STDIN));
+
+$contents = get_contents('https://www.filimo.com/m/' . $video_id);
+preg_match('/"title_movie">\n.*>(.*?)<\/span>/', $contents, $match);
+if (!isset($match[1])) {
+    die("\nSorry! Not found any video with this ID.\n");
+}
+$title = $match[1];
+echo "Title: $title\n";
+
+preg_match('/"movie-time">.*?([0-9]+).*?<\/div>/', $contents, $match);
+$duration = $match[1];
+echo "Duration: $duration min\n";
+
+preg_match('/rateit-current-rate-.*">(.*?)<\/i>/', $contents, $match);
+$rate = $match[1];
+echo "Rate: $rate / 5\n";
+
+echo "===========================================================\n";
+
+$contents = get_contents('https://www.filimo.com/w/' . $video_id);
+preg_match('/{file: "([^{]*?)\?dim=" \+ width \+ "," \+ height, type:"video\/mp4"}/', $contents, $match);
+
+$contents = get_contents($match[1]);
+preg_match_all('/#((?:[0-9])+(?:p|k)+)\n(?:.*)BANDWIDTH=(.*),RESOLUTION=(.*)\n(.*)/', $contents, $matches);
+
+$qualities = array();
+foreach ($matches[1] as $key => $value) {
+    $qualities[] = array(
+        'quality' => $matches[1][$key],
+        'bandwidth' => $matches[2][$key],
+        'resolution' => $matches[3][$key],
+        'url' => $matches[4][$key],
+    );
+}
+
+$qualities = multisort($qualities, 'bandwidth', SORT_ASC);
+$qualities = array_combine(range(1, count($qualities)), array_values($qualities));
+
+echo "Select Quality:\n";
+foreach ($qualities as $key => $value) {
+    echo $key . ") QUALITY=" . $value['quality'] . " - BANDWIDTH=" . $value['bandwidth'] . " - RESOLUTION=" . $value['resolution'] . "\n";
+}
+echo "Input option number: ";
+$input = trim(fgets(STDIN));
+
+@mkdir($base_path);
+$file_name = $video_id . '_' . $qualities[$input]['quality'];
+$video_file = $base_path . $file_name . '.mp4';
+if (isset($proxy) && $proxy) {
+    $cmd_proxy = '-http_proxy http://' . $proxy;
+} else {
+    $cmd_proxy = '';
+}
+$cmd = 'ffmpeg ' . $cmd_proxy . ' -i "' . $qualities[$input]['url'] . '" -y "' . $video_file . '"';
+$log_file = $base_path . $file_name . '.log';
+$info_file = $base_path . $file_name . '.info';
+
+$info = array();
+$info['video_id'] = $video_id;
+$info['title'] = $title;
+$info['duration'] = $duration;
+$info['rate'] = $rate;
+$info['quality'] = $qualities[$input]['quality'];
+$info['bandwidth'] = $qualities[$input]['bandwidth'];
+$info['resolution'] = $qualities[$input]['resolution'];
+$info = json_encode($info);
+file_put_contents($info_file, $info);
+
+if (strtoupper(substr(PHP_OS, 0, 3)) === 'WIN') {
+    pclose(popen('start /B ' . $cmd . '<nul >nul 2>"' . $log_file . '"', 'r'));
+} else {
+    shell_exec($cmd . '</dev/null >/dev/null 2>"' . $log_file . '" &');
+}
+
+echo "===========================================================\n";
+echo "Video file: $video_file\n";
+echo "Log file: $log_file\n";
+echo "Info file: $info_file\n";
+echo "Start downloading in the background...\n";
+echo "You can see stats download with 'stats.php' in the web page.\n";
+echo "For stop process, kill it.\n";
+echo "Bye!\n";
+
+
+function get_contents($url)
+{
+    global $config_path;
+    $ch = curl_init();
+    curl_setopt($ch, CURLOPT_URL, $url);
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+    curl_setopt($ch, CURLOPT_FOLLOWLOCATION, true);
+    curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, false);
+    curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+    curl_setopt($ch, CURLOPT_COOKIEJAR, $config_path . 'cookies.txt');
+    curl_setopt($ch, CURLOPT_COOKIEFILE, $config_path . 'cookies.txt');
+    if (isset($proxy) && $proxy) {
+        list($proxyIp, $proxyPort) = explode(':', $proxy);
+        curl_setopt($ch, CURLOPT_PROXY, $proxyIp);
+        curl_setopt($ch, CURLOPT_PROXYPORT, $proxyPort);
+    }
+    curl_setopt($ch, CURLOPT_HTTPHEADER, array(
+        'Accept:text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8',
+        'Accept-Language:en-US,en;q=0.9',
+        'Cache-Control:max-age=0',
+        'Connection:keep-alive',
+        'Host:www.filimo.com',
+        'Upgrade-Insecure-Requests:1',
+        'User-Agent:Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/63.0.3239.84 Safari/537.36',
+    ));
+    return curl_exec($ch);
+}
+
+function multisort($mdarray, $mdkey, $sort = SORT_ASC)
+{
+    foreach ($mdarray as $key => $row) {
+        // replace 0 with the field's index/key
+        $dates[$key] = $row[$mdkey];
+    }
+    array_multisort($dates, $sort, $mdarray);
+    return $mdarray;
+}
---
+++ stats.php
@@ -0,0 +1,232 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset='UTF-8'>
+    <title>Filimo Downloader Stats</title>
+    <script type="text/javascript">
+        var timeout;
+        document.addEventListener("DOMContentLoaded", function (event) {
+            startReload();
+        });
+
+        function stopReload() {
+            document.getElementById('refresh_status').innerHTML = 'Off';
+            clearTimeout(timeout);
+        }
+
+        function startReload() {
+            document.getElementById('refresh_status').innerHTML = 'On';
+            timeout = setTimeout(function () {
+                window.location.reload();
+            }, 2000);
+        }
+    </script>
+    <script>
+        function sortTable(n) {
+            var table, rows, switching, i, x, y, shouldSwitch, dir, switchcount = 0;
+            table = document.getElementById("myTable");
+            switching = true;
+            // Set the sorting direction to ascending:
+            dir = "asc";
+            /* Make a loop that will continue until
+            no switching has been done: */
+            while (switching) {
+                // Start by saying: no switching is done:
+                switching = false;
+                rows = table.getElementsByTagName("TR");
+                /* Loop through all table rows (except the
+                first, which contains table headers): */
+                for (i = 1; i < (rows.length - 1); i++) {
+                    // Start by saying there should be no switching:
+                    shouldSwitch = false;
+                    /* Get the two elements you want to compare,
+                    one from current row and one from the next: */
+                    x = rows[i].getElementsByTagName("TD")[n];
+                    y = rows[i + 1].getElementsByTagName("TD")[n];
+                    /* Check if the two rows should switch place,
+                    based on the direction, asc or desc: */
+                    if (dir == "asc") {
+                        if (x.innerHTML.toLowerCase() > y.innerHTML.toLowerCase()) {
+                            // If so, mark as a switch and break the loop:
+                            shouldSwitch = true;
+                            break;
+                        }
+                    } else if (dir == "desc") {
+                        if (x.innerHTML.toLowerCase() < y.innerHTML.toLowerCase()) {
+                            // If so, mark as a switch and break the loop:
+                            shouldSwitch = true;
+                            break;
+                        }
+                    }
+                }
+                if (shouldSwitch) {
+                    /* If a switch has been marked, make the switch
+                    and mark that a switch has been done: */
+                    rows[i].parentNode.insertBefore(rows[i + 1], rows[i]);
+                    switching = true;
+                    // Each time a switch is done, increase this count by 1:
+                    switchcount++;
+                } else {
+                    /* If no switching has been done AND the direction is "asc",
+                    set the direction to "desc" and run the while loop again. */
+                    if (switchcount == 0 && dir == "asc") {
+                        dir = "desc";
+                        switching = true;
+                    }
+                }
+            }
+        }
+    </script>
+
+    <style>
+        table {
+            font-size: 20px;
+            width: 100%;
+        }
+
+        td, th {
+            padding: 10px;
+        }
+
+        th {
+            cursor: pointer;
+        }
+    </style>
+</head>
+
+<body>
+<h1>Filimo Downloader Stats</h1>
+
+Refresh:
+<button onclick="startReload();">Start</button>
+<button onclick="stopReload();">Stop</button>
+<span id="refresh_status"></span>
+
+<table border='1' id="myTable">
+    <thead>
+    <tr style='font-weight:bold;'>
+        <th onclick="sortTable(0)">file name</th>
+        <th onclick="sortTable(1)">video id</th>
+        <th onclick="sortTable(2)">title</th>
+        <th onclick="sortTable(3)">rate(x/5)</th>
+        <th onclick="sortTable(4)">duration(min)</th>
+        <th onclick="sortTable(5)">quality</th>
+        <th onclick="sortTable(6)">bandwidth</th>
+        <th onclick="sortTable(7)">resolution</th>
+        <th onclick="sortTable(8)">current file size</th>
+        <th onclick="sortTable(9)">last speed</th>
+        <th onclick="sortTable(10)">progress</th>
+        <th onclick="sortTable(11)">progress bar</th>
+        <th onclick="sortTable(12)">last modified date</th>
+    </tr>
+    </thead>
+    <?php
+    error_reporting(E_ALL & ~E_NOTICE);
+    date_default_timezone_set('Asia/Tehran');
+    $base_path = 'download/';
+    $files = array_filter(glob($base_path . '*.log', GLOB_BRACE), 'is_file');
+    usort($files, create_function('$a,$b', 'return filemtime($b) - filemtime($a);'));
+    foreach ($files as $file) {
+        echo "<tr>\n";
+
+        $dirname = pathinfo($file, PATHINFO_DIRNAME);
+        $filename = pathinfo($file, PATHINFO_FILENAME);
+
+        $filename_log = $dirname . DIRECTORY_SEPARATOR . $filename . '.log';
+        $filename_video = $dirname . DIRECTORY_SEPARATOR . $filename . '.mp4';
+        $filename_info = $dirname . DIRECTORY_SEPARATOR . $filename . '.info';
+
+        $modified_date = date('Y-m-d H:i:s', filemtime($filename_log));
+        if (is_file($filename_video)) {
+            $filesize = filesize_formatted($filename_video);
+        } else {
+            $filesize = 0;
+        }
+
+        echo "<td><a href='$filename_video'>$filename.mp4</a></td>\n";
+
+        $content = @file_get_contents($filename_info);
+        $info = json_decode($content);
+
+        echo "<td>{$info->video_id}</td>\n";
+        echo "<td style='direction:rtl;'><a href='http://www.filimo.com/m/{$info->video_id}' target='_blank'>{$info->title}</a></td>\n";
+        echo "<td>{$info->rate}</td>\n";
+        echo "<td>{$info->duration}</td>\n";
+        echo "<td>{$info->quality}</td>\n";
+        echo "<td>{$info->bandwidth}</td>\n";
+        echo "<td>{$info->resolution}</td>\n";
+        echo "<td>{$filesize}</td>\n";
+
+        $content = @file_get_contents($filename_log);
+
+        //get duration of source
+        preg_match("/Duration: (.*?), start:/", $content, $matches);
+
+        $rawDuration = $matches[1];
+
+        //rawDuration is in 00:00:00.00 format. This converts it to seconds.
+        $ar = array_reverse(explode(":", $rawDuration));
+        $duration = floatval($ar[0]);
+        if (!empty($ar[1])) $duration += intval($ar[1]) * 60;
+        if (!empty($ar[2])) $duration += intval($ar[2]) * 60 * 60;
+
+        //get the time in the file that is already encoded
+        preg_match_all("/time=(.*?) bitrate/", $content, $matches);
+
+        $rawTime = array_pop($matches);
+
+        //this is needed if there is more than one match
+        if (is_array($rawTime)) {
+            $rawTime = array_pop($rawTime);
+        }
+
+        //rawTime is in 00:00:00.00 format. This converts it to seconds.
+        $ar = array_reverse(explode(":", $rawTime));
+        $time = floatval($ar[0]);
+        if (!empty($ar[1])) $time += intval($ar[1]) * 60;
+        if (!empty($ar[2])) $time += intval($ar[2]) * 60 * 60;
+
+        //calculate the progress
+        $progress = round(($time / $duration) * 100, 2);
+
+        //echo "Duration: " . $duration . "\n";
+        //echo "Current Time: " . $time . "\n";
+        //echo "Progress: " . $progress . "%\n";
+
+        preg_match_all("/ speed=(.*?)x/", $content, $matches);
+        $last_speed = array_pop($matches);
+        $last_speed = array_pop($last_speed);
+
+        echo "<td>{$last_speed}x</td>\n";
+        echo "<td>{$progress}%</td>\n";
+        echo "<td><div style='width:100px;height: 20px;border: 1px solid #0095ff;'><div style='width:$progress%; background-color: #0095ff;height: 20px;'></div></div></td>\n";
+
+        echo "<td>$modified_date</td>\n";
+
+
+        echo "</tr>\n";
+    }
+    ?>
+</table>
+
+<pre>
+===========================================================
+Filimo Downloader - version 0.1.0 - Copyright 2017
+By Nabi KaramAliZadeh &lt;www.nabi.ir&gt; &lt;nabikaz@gmail.com&gt;
+Signup here: http://filimo.com/invite/NabiKAZ/a8ca
+Project link: https://github.com/NabiKAZ/filimo-downloader
+===========================================================
+</pre>
+
+</body>
+</html>
+<?php
+function filesize_formatted($path)
+{
+    $size = filesize($path);
+    $units = array('B', 'KB', 'MB', 'GB', 'TB', 'PB', 'EB', 'ZB', 'YB');
+    $power = $size > 0 ? floor(log($size, 1024)) : 0;
+    return number_format($size / pow(1024, $power), 2, '.', ',') . ' ' . $units[$power];
+}
+
+?>
